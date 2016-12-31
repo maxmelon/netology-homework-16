6cba327efa1efe1e7b42647391ed8d4f1d3fcd9e
maxmelon <bh.melon@gmail.com>
2016-12-31 16:59:13 +0500

    Map added

--- classes/YandexGeo.php
+++ classes/YandexGeo.php
@@ -2,6 +2,14 @@
 
 class YandexGeo
 {
+    protected $selector;
+
+    /**
+     * Принимает адрес и возвращает объект, содержащий результаты поиска -
+     * 5 подходящих адресов и их свойства, в том числе координаты.
+     * @param $address
+     * @return \Yandex\Geo\GeoObject[]
+     */
     public function searchPoint($address)
     {
         $api = new \Yandex\Geo\Api();
@@ -12,5 +20,28 @@ class YandexGeo
         $response = $api->getResponse();
         $collection = $response->getList();
         return $collection;
+    }
+
+    /**
+     * Определяет селектор для выбора адреса, который будет отображен на карте.
+     * При значении селектора 0 (по умолчанию) на карте будет показан
+     * первый адрес из результатов поиска.
+     * @return void
+     */
+    public function usersSelection()
+    {
+        if(isset($_GET['result'])) {
+            $this->selector = htmlspecialchars($_GET['result']);
+        } else {
+            $this->selector = 0;
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSelector()
+    {
+        return $this->selector;
     }
 }
--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once 'autoloader.php';
 $geo = new YandexGeo();
 error_reporting(E_CORE_ERROR);
 ?>
+
 <!doctype html>
 <html lang="ru">
 <head>
@@ -11,30 +12,74 @@ error_reporting(E_CORE_ERROR);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>Composer Test: Yandex Geo</title>
 </head>
+
 <body>
-<form method="post">
+<form method="get" style="margin-bottom: 20px">
     <label for="address">Адрес:</label>
     <input name="address" id="address">
     <button type="submit">Найти координаты</button>
 </form>
-<?php if (isset($_POST['address'])) : $collection = $geo->searchPoint($_POST['address']);
-    if (!empty($collection)) : ?>
+
+<?php if (isset($_GET['address'])) :
+    $address = htmlspecialchars($_GET['address']);
+    $collection = $geo->searchPoint($address); ?>
+
+<?php if (!empty($collection)) : ?>
+    <script src="//api-maps.yandex.ru/2.0/?load=package.standard,package.geoObjects&lang=ru-RU" type="text/javascript"></script>
+        <script>
+            <?php $geo->usersSelection(); ?>
+            ymaps.ready(init);
+            var latitude = <?php echo $latitude = $collection[$geo->getSelector()]->getLatitude(); ?>;
+            var longitude = <?php echo $longitude = $collection[$geo->getSelector()]->getLongitude(); ?>;
+            function init () {
+                var myMap = new ymaps.Map("map", {
+                        center: [latitude, longitude],
+                        zoom: 10
+                    }),
+
+                    myGeoObject = new ymaps.GeoObject({
+                        geometry: {
+                            type: "Point",
+                            coordinates: [latitude, longitude]
+                        },
+                        properties: {
+                            iconContent: `${latitude}, ${longitude}`
+                        }
+                    }, {
+                        preset: 'twirl#redStretchyIcon',
+                        draggable: false
+                    });
+
+                myMap.geoObjects
+                    .add(myGeoObject);
+            }
+        </script>
+
+    <div id="map" style="width:400px; height:300px"></div>
+
     <h4>Скорее всего, вы искали координаты для одного из следующих адресов</h4>
     <ul>
-    <?php foreach ($collection as $item) :?>
+    <?php $i = 0; foreach ($collection as $item) :?>
         <li>
             <?php echo $item->getAddress(); ?> -
-            <?php echo $item->getLatitude() . ', ' . $item->getLongitude(); ?>
+            <?php echo $item->getLatitude() . ', ' . $item->getLongitude();
+            if ($item->getLatitude() == $latitude && $item->getLongitude() == $longitude) : ?>
+                <b>Показан на карте</b>
+                <?php else : ?>
+            <a href="index.php?address=<?php echo str_replace(" ", "+", $address) ?>&result=<?php echo $i ?>">Показать на карте</a>
+                <?php endif; ?>
         </li>
-    <?php endforeach; ?>
+    <?php $i++; endforeach; ?>
     </ul>
         Если вы не нашли нужный вам адрес в списке, попробуйте уточнить поисковый запрос.
     <?php else : ?>
     <h4>К сожалению, мы ничего не нашли по вашему запросу :(</h4>
     <?php endif; ?>
     <?php endif; ?>
+
 </body>
 </html>
 
